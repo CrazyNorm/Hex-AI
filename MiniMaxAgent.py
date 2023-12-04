72378499a2bcdd8892d819d9163db0660ca0e430
Spencer Hall <spencer.hall@student.manchester.ac.uk>
2023-12-04 21:22:03 +0000

    updated minimax agent

--- MiniMaxAgent.py
+++ MiniMaxAgent.py
@@ -1,12 +1,12 @@
 import socket
 from random import choice
 from time import sleep
+from Board import Board
+from Tile import Tile
 
 
 class MiniMaxAgent():
-    """This class describes the MiniMax agent.
-    """
-
+    
     HOST = "127.0.0.1"
     PORT = 1234
 
@@ -23,8 +23,7 @@ class MiniMaxAgent():
         self.turn_count = 0
 
     def run(self):
-        """Reads data until it receives an END message or the socket closes."""
-
+        
         while True:
             data = self.s.recv(1024)
             if not data:
@@ -36,10 +35,7 @@ class MiniMaxAgent():
         # print(f"Naive agent {self.colour} terminated")
 
     def interpret_data(self, data):
-        """Checks the type of message and responds accordingly. Returns True
-        if the game ended, False otherwise.
-        """
-
+        
         messages = data.decode("utf-8").strip().split("\n")
         messages = [x.split(";") for x in messages]
         # print(messages)
@@ -152,39 +148,87 @@ class MiniMaxAgent():
                         break
         return min_score
     
-    def getNeighbours(self,x,y):
-        #returns  a list of the neighbours of the given board index 2 to 6 neighbours possible
+    def getNeighbours(self, x, y):
         
+        neighbours = []
 
-        pass
+        for i in range(Tile.NEIGHBOUR_COUNT):
+            x_n = x + Tile.I_DISPLACEMENTS[i]
+            y_n = y + Tile.J_DISPLACEMENTS[i]
+
+            #check if the calculated coordinates are within the bounds of the board
+            if 0 <= x_n < self.board_size and 0 <= y_n < self.board_size:
+                neighbours.append((x_n, y_n))
+
+        return neighbours
 
     def checkIfFinished(self):
-        #Check if either player has won
+        
+        #check if chain of blues from left to right
+        for i in range(self.board_size):
+            if self.board[0][i] == "B":
+                visited = set()
+                if self.DFS_check(0, i, "B", visited):
+                    return -1  # Blue wins
 
-            #Check if chain of blues from left to right, 
+        #check if chain of reds from top to bottom
+        for i in range(self.board_size):
+            if self.board[i][0] == "R":
+                visited = set()
+                if self.DFS_check(i, 0, "R", visited):
+                    return 1  # Red wins
 
-            #Check if chain of reds from top to bottom,
+        return 0  #no winner yet
+    
+    def DFS_check(self, x, y, color, visited):
 
-        #If blue wins, return -1, red wins return 1, if no winner yet return 0
+        visited.add((x, y))
 
-        pass
+        #if the chain reaches the opposite side, return True
+        if (color == "B" and y == self.board_size - 1) or (color == "R" and x == self.board_size - 1):
+            return True
+
+        #visit neighbors
+        for nx, ny in self.getNeighbours(x, y):
+            if (nx, ny) not in visited and self.board[nx][ny] == color:
+                if self.DFS_check(nx, ny, color, visited):
+                    return True
+
+        return False
 
     def evaluate_board(self):
-        #First check if there is a winner
-
-        #Number of plays opponent needs to win - Number of plays agent needs to win
-
-
-        pass
-
-
     
+        #check if there is already a winner
+        winner = self.checkIfFinished()
+        if winner != 0:
+            return winner * float('inf') 
+
+        #number of plays opponent needs to win - Number of plays agent needs to win
+        num_plays_opponent = self.count_plays_to_win(self.opp_colour())
+        num_plays_agent = self.count_plays_to_win(self.colour)
+
+        return num_plays_opponent - num_plays_agent
+
+
+    def count_plays_to_win(self, color):
+        target_row = self.board_size - 1 if color == "R" else None
+        target_col = self.board_size - 1 if color == "B" else None
+
+        plays_needed = 0
+
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.board[i][j] == color:
+                    #calculate the minimum distance to the target row or column
+                    distance_to_target = min(abs(i - target_row), abs(j - target_col))
+                    plays_needed = max(plays_needed, distance_to_target)
+
+        return plays_needed
+
 
 
     def opp_colour(self):
-        """Returns the char representation of the colour opposite to the
-        current one.
-        """
+        
         if self.colour == "R":
             return "B"
         elif self.colour == "B":
